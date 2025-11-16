aa613ca9ef241957c788ab32d4649457efa8ae9a
shaban <shabanshaikh72345@gmail.com>
2025-11-16 19:13:44 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' show  Vector3;
 
 void main() {
   runApp(const MyApp());
@@ -13,21 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -38,85 +27,144 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
+
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+const double widthAndHeight = 100;
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin  {
+ late AnimationController  _xController;
+ late AnimationController _yController;
+ late AnimationController _zController;
+ late Tween<double> _animation;
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+ @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _xController = AnimationController(
+        vsync: this,
+     duration: const Duration(seconds: 20)
+    );
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    _yController = AnimationController(
+        vsync: this,
+        duration: const Duration(seconds: 30)
+    );
+
+
+    _zController = AnimationController(
+        vsync: this,
+        duration: const Duration(seconds: 40)
+    );
+
+    _animation = Tween<double>(
+      begin: 0,
+      end: pi  *2
+    );
+
   }
 
   @override
+  void dispose() {
+    // TODO: implement dispose
+    _xController.dispose();
+    _yController.dispose();
+    _zController.dispose();
+    super.dispose();
+  }
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+   _xController..reset()..repeat();
+   _yController..reset()..repeat();
+   _zController..reset()..repeat();
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+     backgroundColor: Colors.transparent,
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: widthAndHeight,
+             width: double.infinity,
+            ),
+            AnimatedBuilder(
+              animation: Listenable.merge([
+                _xController,
+                _yController,
+                _zController
+              ]),
+              builder: (context,child){
+                return Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.identity()..rotateX(_animation.evaluate(_xController))
+                    ..rotateY(_animation.evaluate(_yController))
+                    ..rotateZ(_animation.evaluate(_zController)),
+                  child: Stack(
+                    children: [
+                      // front
+
+                      Transform(
+                        alignment: Alignment.center,
+                        transform: Matrix4.identity()..translate(Vector3(0, 0, widthAndHeight)),
+                        child: Container(
+                          color: Colors.purple,
+                          width: widthAndHeight,
+                          height: 100,
+                        ),
+                      ),
+                      Transform(
+                        alignment: Alignment.centerRight,
+                        transform: Matrix4.identity()..rotateY(pi/2),
+                        child: Container(
+                          color: Colors.red,
+                          width: widthAndHeight,
+                          height: 100,
+                        ),
+                      ),
+                      Transform(
+                        alignment: Alignment.centerLeft,
+                        transform: Matrix4.identity()..rotateY(-pi/2),
+                        child: Container(
+                          color: Colors.yellow,
+                          width: widthAndHeight,
+                          height: 100,
+                        ),
+                      ),
+
+                      Container(
+                        color: Colors.green,
+                        width: widthAndHeight,
+                        height: 100,
+                      ),
+                      Transform(
+                        alignment: Alignment.topCenter,
+                        transform: Matrix4.identity()..rotateX(pi/2),
+                        child: Container(
+                          color: Colors.deepOrangeAccent,
+                          width: widthAndHeight,
+                          height: 100,
+                        ),
+                      ),
+                      Transform(
+                        alignment: Alignment.bottomCenter,
+                        transform: Matrix4.identity()..rotateX(-pi/2),
+                        child: Container(
+                          color: Colors.tealAccent,
+                          width: widthAndHeight,
+                          height: 100,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
     );
   }
 }
